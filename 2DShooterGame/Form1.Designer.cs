05a3730ba4a1fde630f9876650d28e61841a1be5
tman6290 <tofaratifarinu@gmail.com>
2023-09-08 11:28:16 +0100

    I've added stars to the background and a timer component to control how fast the background moves.

--- 2DShooterGame/Form1.Designer.cs
+++ 2DShooterGame/Form1.Designer.cs
@@ -37,11 +37,13 @@
             // 
             this.MoveBgTimer.Enabled = true;
             this.MoveBgTimer.Interval = 10;
+            this.MoveBgTimer.Tick += new System.EventHandler(this.MoveBgTimer_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
             this.ClientSize = new System.Drawing.Size(584, 461);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximumSize = new System.Drawing.Size(600, 500);
--- 2DShooterGame/Form1.cs
+++ 2DShooterGame/Form1.cs
@@ -22,7 +22,51 @@ namespace _2DShooterGame
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            backgroundSpeed = 4;
+            stars = new PictureBox[10];
+            random = new Random();
 
+            for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i] = new PictureBox();
+                stars[i].BorderStyle = BorderStyle.None;
+                stars[i].Location = new Point(random.Next(20, 580), random.Next(-10, 400));
+                if (i % 2 == 1)
+                {
+                    stars[i].Size = new Size(2, 2);
+                    stars[i].BackColor = Color.Wheat;
+                }
+                else
+                {
+                    stars[i].Size = new Size(3, 3);
+                    stars[i].BackColor = Color.DarkGray;
+                }
+
+                this.Controls.Add(stars[i]);
+
+            }
+        }
+
+        private void MoveBgTimer_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < stars.Length / 2; i++)
+            {
+                stars[i].Top += backgroundSpeed;
+                if (stars[i].Top >= this.Height)
+                {
+                    stars[i].Top = -stars[i].Height;
+                }
+            }
+
+            for (int i = stars.Length / 2; i < stars.Length; i++)
+            {
+                stars[i].Top += backgroundSpeed - 2;
+
+                if (stars[i].Top >= this.Height)
+                {
+                    stars[i].Top = -stars[i].Height;
+                }
+            }
         }
     }
 }
